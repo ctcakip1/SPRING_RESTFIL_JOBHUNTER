ceeb59a0480cee093edcddf77609064daafdf565
ctcakip1 <tinigaming058@gmail.com>
2024-07-11 16:07:56 +0700

    hello world with gmail

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -46,7 +46,7 @@ public class SecurityConfiguration {
 
         String[] whiteList = { "/",
                 "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/register", "/storage/**",
-                "/api/v1/companies/**", "/api/v1/jobs/**" };
+                "/api/v1/companies/**", "/api/v1/jobs/**", "/api/v1/email/**" };
 
         http
                 .csrf(c -> c.disable())
---
+++ src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
@@ -0,0 +1,28 @@
+package vn.hoidanit.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.jobhunter.service.EmailService;
+import vn.hoidanit.jobhunter.util.anotation.ApiMessage;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmailController {
+    private final EmailService emailService;
+
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/email")
+    @ApiMessage("send simple email")
+    public String sendSimpleEmail() {
+        this.emailService.sendSimpleEmail();
+        return "ok";
+    }
+
+}
